b695796958bd40dd35510e3d21cafe461402b76c
stringsout <76243098+FebykAlekSatria@users.noreply.github.com>
2022-07-09 13:55:19 +0700

    Call all component

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
 
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Navbar from '../components/Navbar';
 import { getData } from '../slices/dataSlice';
-import { getUser } from '../slices/userSlice';
-// import dataSoal from './features/soaltest';
+import CardProfile from '../components/cards/CardProfile';
+import CardListRepo from '../components/cards/CardListRepo';
+import CardResume from '../components/cards/CardResume';
 
 export default function Home() {
   const dispatch = useDispatch();
-  const  data = useSelector(state => state.data.data);
-  const  user = useSelector(state => state.user.data);
+
   useEffect(() => {
     dispatch(getData());
-    dispatch(getUser());
   }, [dispatch]);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      {/* {data} */}
-    </h1>
+    <div className="bg-primary font-poppins">
+      <Navbar />
+      <div className="lg:grid lg:grid-cols-4 gap-4 m-4">
+        <div className="">
+          <CardProfile/>
+        </div>
+        <div className="my-4 md:my-0 col-span-2">
+            <CardListRepo/>
+        </div>
+        <div className="my-4 md:my-0">
+          <CardResume/>
+        </div>
+      </div>
+  </div>
   )
 }
