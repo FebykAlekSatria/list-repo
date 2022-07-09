80a387a138c64d763864ca0dc151396dca6587b1
stringsout <76243098+FebykAlekSatria@users.noreply.github.com>
2022-07-09 13:36:09 +0700

    isLoading
    content loading if isLoading true from store

--- components/cards/CardProfile.js
+++ components/cards/CardProfile.js
@@ -1,15 +1,12 @@
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { AiTwotoneMail } from 'react-icons/ai';
-import { BsTelephoneFill } from 'react-icons/bs';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '../../slices/userSlice';
 
 export default function CardProfile(){
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleModal = () => setIsOpen(!isOpen);
     const dispatch = useDispatch();
     const user = useSelector(state => state.user.data);
+    const isLoading = useSelector(state => state.user.isLoading);
 
     useEffect(() => {
         dispatch(getUser());
@@ -18,23 +15,27 @@ export default function CardProfile(){
     
     return(
         <div className="flex flex-row md:block rounded-xl shadow-md bg-white-primary md:text-center py-6 px-4 md:px-0 md:items-center">
-            <div className="w-1/3 md:w-full">
-              <img className="md:w-32 md:h-32 w-24 h-24 md:mx-auto mx-2 rounded-full shadow" alt="profile" src={user.avatar_url}/>
-            </div>
-            <div className="w-2/3 md:w-full gap-2">
-              <p className="font-bold mb-2">{user.name}</p>
-              <a href={user.blog} className="text-gray-icon text-sm mb-2">{user.blog}</a>
-              <p className="text-blue-primary text-sm mb-2">{user.location}</p>
-              {user.html_url &&
-                <Link href={user.html_url} > 
-                  <a> 
-                    <p className="w-1/3 bg-blue-primary rounded-md p-2 px-4 font-bold md:mx-auto my-4 text-white">Github </p> 
-                  </a> 
-                </Link>
-              }
-              
-            </div>
-          </div>
+          {isLoading ? <div className="text-center">Loading...</div> : 
+            <>
+              <div className="w-1/3 md:w-full">
+                <img className="md:w-32 md:h-32 w-24 h-24 md:mx-auto mx-2 rounded-full shadow" alt="profile" src={user.avatar_url}/>
+              </div>
+              <div className="w-2/3 md:w-full gap-2">
+                <p className="font-bold my-2">{user.name}</p>
+                <a href={user.blog} className="text-gray-icon text-sm mb-2">{user.blog}</a>
+                <p className="text-blue-primary text-sm mb-2">{user.location}</p>
+                {user.html_url &&
+                  <Link href={user.html_url} > 
+                    <a> 
+                      <p className="w-1/3 bg-blue-primary rounded-md p-2 px-4 font-bold md:mx-auto my-4 text-white">Github </p> 
+                    </a> 
+                  </Link>
+                }
+                
+              </div>
+            </>
+          }
+        </div>
 
     )
 }
--- components/cards/CardResume.js
+++ components/cards/CardResume.js
@@ -6,7 +6,8 @@ import { useSelector } from "react-redux";
 
 export default function CardResume(){
     const data = useSelector(state => state.data.data);
-    // count data language javascript
+    const isLoading = useSelector(state => state.data.isLoading);
+
     const js = data.filter(item => item.language === "JavaScript").length;
     const python = data.filter(item => item.language === "Python").length;
     const php = data.filter(item => item.language === "PHP").length;
@@ -14,14 +15,18 @@ export default function CardResume(){
 
     return(
         <div className="rounded-xl shadow-md bg-white-primary p-4 md:p-6 text-sm mb-4 ">
-            <p className="font-bold text-blue-primary">Indikator OPD</p>
-            <div className="flex flex-wrap gap-2 my-2">
-                <button className="flex bg-orange-primary text-white rounded px-2 py-1 gap-1"><MdVisibility className="my-auto"/>{total}</button>
-                <button className="flex bg-orange-primary text-white rounded px-2 py-1 gap-1"><MdVisibilityOff className="my-auto"/>6</button>
-                <button className="flex bg-blue-secondary text-white rounded px-2 py-1 gap-1"> <SiPhp className="my-auto"/>{php}</button>
-                <button className="flex bg-red-primary text-white rounded px-2 py-1 gap-1"><SiPython className="my-auto"/>{python}</button>
-                <button className="flex bg-blue-400 text-white rounded px-2 py-1 gap-1"> <SiJavascript className="my-auto"/>{js}</button>
-            </div>
+            {isLoading ? <div className="text-center">Loading...</div> :
+                <>
+                    <p className="font-bold text-blue-primary">Indikator OPD</p>
+                    <div className="flex flex-wrap gap-2 my-2">
+                        <button className="flex bg-orange-primary text-white rounded px-2 py-1 gap-1"><MdVisibility className="my-auto"/>{total}</button>
+                        <button className="flex bg-orange-primary text-white rounded px-2 py-1 gap-1"><MdVisibilityOff className="my-auto"/>6</button>
+                        <button className="flex bg-blue-secondary text-white rounded px-2 py-1 gap-1"> <SiPhp className="my-auto"/>{php}</button>
+                        <button className="flex bg-red-primary text-white rounded px-2 py-1 gap-1"><SiPython className="my-auto"/>{python}</button>
+                        <button className="flex bg-blue-400 text-white rounded px-2 py-1 gap-1"> <SiJavascript className="my-auto"/>{js}</button>
+                    </div>
+                </>
+            }
           </div>
     )
 }
