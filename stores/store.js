fe0d30e0d123cc48c8559e4ac9cd773110ef407e
stringsout <76243098+FebykAlekSatria@users.noreply.github.com>
2022-07-09 10:43:16 +0700

    Create Store
    data user and data repo

--- stores/store.js
+++ stores/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import data from '../slices/dataSlice';
+import user from '../slices/userSlice';
 
 export const store = configureStore({
   reducer: {
-    data: data
+    data: data,
+    user:user
   }
 });
